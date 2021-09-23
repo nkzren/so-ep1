b33676394c398d489ff81687e4a14793b0452c9b
nkzren <renan.nakazawa0@gmail.com>
2021-09-23 20:40:18 -0300

    adiciona leitura do quantum e extrai para um metodo de instancia

--- src/main/java/br/usp/Main.java
+++ src/main/java/br/usp/Main.java
@@ -1,5 +1,8 @@
 package br.usp;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -9,21 +12,36 @@ import java.util.stream.Stream;
 
 public class Main {
 
-    public String hello() {
-        return "Hello World";
-    }
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
-    public static void main(String[] args) throws IOException {
-        System.out.println(new Main().hello());
+    public void init() {
+        try {
+            int quantum = Integer.parseInt(Files.readString(Paths.get(BASE_PATH + "quantum.txt")).replaceAll("\n", ""));
+            System.out.println("Quantum: " + quantum);
+        } catch (IOException e) {
+            LOGGER.error("Erro na leitura do quantum: ", e);
+        }
 
         System.out.println("Escolha um arquivo para ler");
         Scanner scanner = new Scanner(System.in);
 
         String test = scanner.next();
-        Stream<String> lines = Files.lines(Paths.get("src/main/resources/programas/" + test), StandardCharsets.UTF_8);
+        try {
+            Stream<String> lines = Files.lines(Paths.get("src/main/resources/programas/" + test), StandardCharsets.UTF_8);
 
-        lines.forEach(line -> {
-            System.out.println("Lendo linha: " + line);
-        });
+            lines.forEach(line -> {
+                System.out.println("Lendo linha: " + line);
+            });
+        } catch (IOException e) {
+            LOGGER.error("Erro na leitura do arquivo selecionado " + test + ": ", e);
+        }
+    }
+
+    private static final String BASE_PATH = "src/main/resources/programas/";
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        main.init();
     }
 }
