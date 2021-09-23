bf7f52a579acd78c48e79ccb9cc15cfa72a600fe
nkzren <renan.nakazawa0@gmail.com>
2021-09-23 20:17:14 -0300

    adiciona arquivos exemplo e leitor de arquivo

--- src/main/java/br/usp/Main.java
+++ src/main/java/br/usp/Main.java
@@ -1,12 +1,29 @@
 package br.usp;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Main {
 
     public String hello() {
         return "Hello World";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(new Main().hello());
+
+        System.out.println("Escolha um arquivo para ler");
+        Scanner scanner = new Scanner(System.in);
+
+        String test = scanner.next();
+        Stream<String> lines = Files.lines(Paths.get("src/main/resources/programas/" + test), StandardCharsets.UTF_8);
+
+        lines.forEach(line -> {
+            System.out.println("Lendo linha: " + line);
+        });
     }
 }
